840f9a53c20c700bd1e7ffe226fa71f0f6c438c4
fecarolb8 <fephn@hotmail.com>
2025-04-30 20:24:32 -0300

    Add files via upload

---
+++ ex 4.cpp
@@ -0,0 +1,16 @@
+#include <iostrem>	
+using namespace std;
+
+int main()
+{
+	double b;
+	double a;
+	double area;
+	cin << "Informe o valor a base o triangolo: ";
+	cout >> b;
+	cin >> a; 
+	area = (b * a) / 2; 
+	cout << "A area caalculaaaa o triaangulo foi " << areaa;
+		
+	return 0;
+}
